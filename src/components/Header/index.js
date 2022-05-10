07f1f38114bd85d6e3496c32eb533e1f11d21c92
onlyphantom <samuel@outlook.sg>
2022-05-10 22:59:18 +0700

    set up w/tailwind

---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <section className="p-4 text-2xl font-bold bg-black text-white mb-4">
+            <div>Generations</div>
+        </section>
+    );
+}
