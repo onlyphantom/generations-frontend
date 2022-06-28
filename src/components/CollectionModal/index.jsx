575f83700f9c53ead03765e638de730657f8ecb5
onlyphantom <samuel@outlook.sg>
2022-06-28 14:17:28 +0700

    mentor social icons

--- src/components/CollectionModal/index.jsx
+++ src/components/CollectionModal/index.jsx
@@ -54,7 +54,29 @@ const CollectionModal = ({ collectionId }) => {
                   </div>
                 ))
             ) : (
-              <p>No bookmarks in this collection</p>
+              <>
+                <svg
+                  className="mx-auto h-12 w-12 text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    vectorEffect="non-scaling-stroke"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+                  />
+                </svg>
+                <h3 className="mt-2 text-sm font-medium text-gray-300">
+                  Empty Collection
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">
+                  This Collection may yet to be updated.
+                </p>
+              </>
             )}
             <div className="modal-action">
               <label htmlFor={collectionId} className="btn">
