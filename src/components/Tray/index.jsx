a634ea1bf885b04d3fc6ab53476cf7a63a4d63e8
sc <samuel@outlook.sg>
2022-08-22 17:51:16 +0700

    TrayContent skeleton

--- src/components/Tray/index.jsx
+++ src/components/Tray/index.jsx
@@ -4,8 +4,9 @@ import { UserContext } from "../../contexts/UserContext";
 
 import TrayButton from "./TrayButton";
 import TrayDrawer from "./TrayDrawer";
-import TrayCard from "./TrayCard";
 import NotLoggedIn from "./NotLoggedIn";
+import TrayContent from "./TrayContent";
+// import TrayCard from "./TrayCard";
 
 const Tray = () => {
   const [trayOpen, setTrayOpen] = useState(false);
@@ -17,19 +18,15 @@ const Tray = () => {
   return (
     <div>
       {!trayOpen && <TrayButton setTrayOpen={setTrayOpen} />}
-      
+
 
       <TrayDrawer trayOpen={trayOpen} setTrayOpen={setTrayOpen}>
         {
-          !user.token && <NotLoggedIn />
+          !user.token
+            ? <NotLoggedIn />
+            : <TrayContent tray={tray} setTray={setTray} />
         }
-        <TrayCard setTray={setTray} />
-        <TrayCard setTray={setTray} />
-        <TrayCard setTray={setTray} />
-
-        <p className="text-xs text-gray-500">{JSON.stringify(tray)}</p>
-
-
+        {/* <TrayCard setTray={setTray} /> */}
         <p className="text-xs text-gray-500">
           {JSON.stringify(user.token)} |{" "}
           {user.token ? "Logged in" : "Not logged in"}
