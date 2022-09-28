99efa25b220644bdd211987d75cdca10a6bc1f74
onlyphantom <samuel@outlook.sg>
2022-09-28 15:41:47 +0700

    styling UserProgress and handle 0 points logic better

--- src/components/Tray/UserProgress.jsx
+++ src/components/Tray/UserProgress.jsx
@@ -3,27 +3,44 @@ import CardEffortPoints from "../CollectionCard/CardEffortPoints";
 import CardTags from "../CollectionCard/CardTags";
 
 const diagonalLine = {
-  borderBottom: "1px solid red",
-  width: "90%",
-  transform: "rotate(17deg)",
+  borderBottom: "1px solid var(--tw-prose-body)",
+  width: "100%",
+  transform: "rotate(7deg)",
   transformOrigin: "top left",
 };
 
 const UserProgress = ({ tagAwards, expendedEffort }) => {
+  const RadialProgess = () => {
+    if (expendedEffort > 0) {
+      return (
+        <div
+          className="radial-progress text-accent"
+          style={{ "--value": expendedEffort * 4 }}
+        >
+          {`${expendedEffort * 4}%`}
+        </div>
+      );
+    } else {
+      return (
+        <p className="prose prose-slate prose-sm leading-snug mr-2">
+          <small>
+            As you complete lessons, your mentor will reward you with points and
+            competency tags that will appear here.
+          </small>
+        </p>
+      );
+    }
+  };
+
   return (
     <div className="rounded-lg m-4 border-accent border-2">
       <div className="mb-4 mt-2">
         <h2 className="prose prose-lg px-4">
-          Expended Effort{" "}
+          Scoreboard{" "}
           <div className="badge badge-md badge-accent">{expendedEffort}</div>
           <div className="flex flex-row">
             <div className="basis-3/5">
-              <div
-                className="radial-progress text-accent"
-                style={{ "--value": 70 }}
-              >
-                70%
-              </div>
+              <RadialProgess />
             </div>
             <div className="basis-2/5">
               {expendedEffort > 0 ? (
@@ -36,11 +53,7 @@ const UserProgress = ({ tagAwards, expendedEffort }) => {
                   <div style={diagonalLine}></div>
                   <CardEffortPoints effort={20} />
                   <p className="prose prose-slate prose-sm leading-snug">
-                    <small>
-                      You haven't accummulated any points. As you complete
-                      lessons, your mentor will reward you with points and
-                      competency tags that will appear here.
-                    </small>
+                    <small>You haven't accummulated any points.</small>
                   </p>
                 </>
               )}
