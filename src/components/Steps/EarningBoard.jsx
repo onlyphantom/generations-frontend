5ac0b8ac4bb51f59291614884566b0a5e2bd4914
onlyphantom <samuel@outlook.sg>
2023-02-04 17:15:07 +0700

    add earning history modal

--- src/components/Steps/EarningBoard.jsx
+++ src/components/Steps/EarningBoard.jsx
@@ -1,11 +1,53 @@
 import { useState, useEffect } from "react";
 
-const EarningHistory = ({ sortedEarning, setShowHistory }) => {
-  return <div>Earning History</div>;
+const EarningHistory = ({ sortedEarning }) => {
+  return (
+    <div>
+      <input
+        type="checkbox"
+        id="earning-history-modal"
+        className="modal-toggle"
+      />
+      <div className="modal">
+        <div className="modal-box relative">
+          <div className="modal-header my-4">
+            <h2 className="modal-title text-2xl">Earning History</h2>
+            <label
+              htmlFor="earning-history-modal"
+              className="btn btn-sm btn-circle absolute right-2 top-2"
+            >
+              ✕
+            </label>
+          </div>
+          <div className="modal-body">
+            <div className="overflow-x-auto">
+              <table className="table table-compact table-zebra w-full">
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Amount (IDR)</th>
+                    <th>Notes</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {sortedEarning.map((earning) => (
+                    <tr key={earning.id}>
+                      <td>{earning.createdAt.slice(0, 10)}</td>
+                      <td>{Number(earning.amount).toLocaleString()}</td>
+                      <td>{earning.type}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export function EarningBoard(user) {
-  const [showHistory, setShowHistory] = useState(false);
   const [sortedEarning, setSortedEarning] = useState(user.earnings);
 
   useEffect(() => {
@@ -35,18 +77,13 @@ export function EarningBoard(user) {
               .toLocaleString()}
           </div>
           <div className="stat-actions">
-            <button className="btn btn-sm" onClick={() => setShowHistory(true)}>
+            <label htmlFor="earning-history-modal" className="btn btn-sm">
               View History
-            </button>
+            </label>
           </div>
         </div>
       </div>
-      {showHistory && (
-        <EarningHistory
-          sortedEarning={sortedEarning}
-          setShowHistory={setShowHistory}
-        />
-      )}
+      <EarningHistory sortedEarning={sortedEarning} />
     </div>
   );
 }
