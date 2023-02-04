4e763b2b1e91e576052a43f4668680ccdcf52e86
onlyphantom <samuel@outlook.sg>
2023-02-04 11:29:43 +0700

    timeline icons

--- src/components/Steps/Fellowship.jsx
+++ src/components/Steps/Fellowship.jsx
@@ -25,11 +25,11 @@ const TimelineIcon = ({ eventType }) => {
   switch (eventType) {
     case "bookmarked":
       return (
-        <span className="absolute flex items-center justify-center w-8 h-8 bg-primary rounded-full -left-4">
+        <span className="absolute flex items-center justify-center w-8 h-8  rounded-full -left-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
-            fill="evenodd"
+            fill="cyan"
             className="w-5 h-5"
           >
             <path
@@ -41,21 +41,53 @@ const TimelineIcon = ({ eventType }) => {
         </span>
       );
 
-    default:
+    case "ongoing":
       return (
-        <span className="absolute flex items-center justify-center w-8 h-8 bg-black rounded-full -left-4">
+        <span className="absolute flex items-center justify-center w-8 h-8 rounded-full -left-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
             viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
+            <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z" />
+            <path d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z" />
+          </svg>
+        </span>
+      );
+
+    case "completed":
+      return (
+        <span className="absolute flex items-center justify-center w-8 h-8 bg-success rounded-full -left-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="black"
+            className="w-5 h-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </span>
+      );
+
+    default:
+      return (
+        <span className="absolute flex items-center justify-center w-8 h-8 bg-info rounded-full -left-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="black"
             className="w-4 h-4"
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5"
+              fillRule="evenodd"
+              d="M3 2.25a.75.75 0 01.75.75v.54l1.838-.46a9.75 9.75 0 016.725.738l.108.054a8.25 8.25 0 005.58.652l3.109-.732a.75.75 0 01.917.81 47.784 47.784 0 00.005 10.337.75.75 0 01-.574.812l-3.114.733a9.75 9.75 0 01-6.594-.77l-.108-.054a8.25 8.25 0 00-5.69-.625l-2.202.55V21a.75.75 0 01-1.5 0V3A.75.75 0 013 2.25z"
+              clipRule="evenodd"
             />
           </svg>
         </span>
@@ -102,6 +134,9 @@ const Fellowship = () => {
       ])
     );
     val.sort((a, b) => b[0].localeCompare(a[0]));
+
+    // drop "requested" events
+    val = val.filter((x) => x[1] !== "requested");
 
     // map first element of each array in val to timeAgo
     val = val.map((x) => {
@@ -209,9 +244,10 @@ const Fellowship = () => {
         <ol className="relative border-l border-gray-200 dark:border-gray-700">
           {timelineEvents.map((event, index) => {
             return (
-              <li className="mb-10 ml-7 even:bg-gray-600" key={index}>
+              <li className="mb-10 ml-7" key={index}>
                 <TimelineIcon eventType={event[1]} />
 
+                {/* <div className="items-center justify-between p-4 border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600"></div> */}
                 <div
                   className={`items-center justify-between p-4 border border-gray-200 rounded-lg shadow-sm sm:flex ${
                     event[1] === "completed" &&
