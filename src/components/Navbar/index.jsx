2404ce5e9c9b3a2c976ff23baa5c11c6771cc914
onlyphantom <samuel@outlook.sg>
2023-02-09 12:26:35 +0700

    prettier profilebox

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -9,6 +9,53 @@ export const scrollToSection = (sectionName) => {
   section.scrollIntoView({ behavior: "smooth", block: "start" });
 };
 
+const ProMembershipStatus = ({ user }) => {
+  if (user.proUser) {
+    return (
+      <li className="text-slate-400">
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+            />
+          </svg>
+          Pro User <br />
+          Expires: {user.proExpiry}
+        </span>
+      </li>
+    );
+  } else {
+    return (
+      <li>
+        <button className="btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 2a6 6 0 110 12 6 6 0 010-12zm0 3a1 1 0 00-1 1v4a1 1 0 002 0V8a1 1 0 00-1-1zm0 6a1 1 0 100-2 1 1 0 000 2z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <span>Upgrade to Pro</span>
+        </button>
+      </li>
+    );
+  }
+};
+
 const Navbar = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
@@ -34,7 +81,7 @@ const Navbar = () => {
           <label tabIndex="0" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
+              className="h-4 w-4"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -124,7 +171,10 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-compact dropdown-content p-2 text-sm shadow bg-base-100 rounded-box w-52 mt-4"
             >
-              <li className="disabled">
+              <li className="menu-title">
+                <span>Account Information</span>
+              </li>
+              <li className="text-slate-400">
                 <span>
                   <a>
                     <svg
@@ -145,6 +195,7 @@ const Navbar = () => {
                   {user.username}
                 </span>
               </li>
+              <ProMembershipStatus user={user} />
 
               {/* separator */}
               <li className="divider"></li>
