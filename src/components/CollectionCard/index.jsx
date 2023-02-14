cf5c875ccdbca412764e30d58df268f117bc824a
onlyphantom <samuel@outlook.sg>
2023-02-14 19:29:01 +0700

    add ribbon to promote onboarding to new users

--- src/components/CollectionCard/index.jsx
+++ src/components/CollectionCard/index.jsx
@@ -9,6 +9,7 @@ import CardEffortPoints from "./CardEffortPoints";
 
 import addOrRemoveFromTray from "../Tray/addOrRemoveFromTray";
 import CardActions from "./CardActions";
+import { specialCollections } from "../../utils/constants";
 
 const CollectionCard = ({ attributes, id }) => {
   const { u, c, bc } = useContext(UserContext);
@@ -58,58 +59,75 @@ const CollectionCard = ({ attributes, id }) => {
     dateRef.current = isoDate.substring(0, isoDate.indexOf("T"));
   }, [id, bookmarkedCollections, attributes.updatedAt]);
 
-  return (
-    <div
-      className="block rounded-lg shadow-lg bg-base-200 p-6 bookmark-card  
+  const mainCardBlock = () => {
+    return (
+      <div
+        className="block rounded-lg shadow-lg bg-base-200 p-6 bookmark-card  
       "
-    >
-      <h5 className="text-2xl font-bold mb-2">
-        <label htmlFor={id} className="link">
-          {attributes.title}
-        </label>
-      </h5>
-      <div className="text-gray-500 mb-4 text-sm markdown-para">
-        <small>Last Updated on {dateRef.current}</small>
-        {attributes.details && <CollectionDetails attributes={attributes} />}
-        {attributes.experts && (
-          <CollectionMentors mentors={attributes.experts.data} />
-        )}
-      </div>
-      <div className="flex flex-row">
-        <div className="basis-4/5">
-          <CardActions
-            bookmarkIcon={bookmarkIcon}
-            num_materials={numMaterials}
-            tagsCount={tagsCount}
-            handleAddToTray={handleAddToTray}
-            trayStatus={selectedTray?.status}
-          />
+      >
+        <h5 className="text-2xl font-bold mb-2">
+          <label htmlFor={id} className="link">
+            {attributes.title}
+          </label>
+        </h5>
+        <div className="text-gray-500 mb-4 text-sm markdown-para">
+          <small>Last Updated on {dateRef.current}</small>
+          {attributes.details && <CollectionDetails attributes={attributes} />}
+          {attributes.experts && (
+            <CollectionMentors mentors={attributes.experts.data} />
+          )}
         </div>
-        <div className="basis-1/5">
-          <div className="tooltip" data-tip="Effort Points">
-            <CardEffortPoints
-              effort={attributes.totalEffort}
-              extraClass="badge-info"
+        <div className="flex flex-row">
+          <div className="basis-4/5">
+            <CardActions
+              bookmarkIcon={bookmarkIcon}
+              num_materials={numMaterials}
+              tagsCount={tagsCount}
+              handleAddToTray={handleAddToTray}
+              trayStatus={selectedTray?.status}
             />
           </div>
+          <div className="basis-1/5">
+            <div className="tooltip" data-tip="Effort Points">
+              <CardEffortPoints
+                effort={attributes.totalEffort}
+                extraClass="badge-info"
+              />
+            </div>
+          </div>
         </div>
-      </div>
 
-      <CollectionModal
-        collectionId={id}
-        showSubmitButton={
-          // Collections title with _ are "special" collections
-          // that can be submitted without a mentor
-          (attributes?.title.startsWith("_") &&
-            selectedTray?.status !== "completed" && user?.token) ||
-          selectedTray?.status === "ongoing"
-            ? true
-            : false
-        }
-        user={attributes?.title.startsWith("_") ? user : null}
-      />
-    </div>
-  );
+        <CollectionModal
+          collectionId={id}
+          showSubmitButton={
+            // Collections title with _ are "special" collections
+            // that can be submitted without a mentor
+            (attributes?.title.startsWith("_") &&
+              selectedTray?.status !== "completed" &&
+              user?.token) ||
+            selectedTray?.status === "ongoing"
+              ? true
+              : false
+          }
+          user={attributes?.title.startsWith("_") ? user : null}
+        />
+      </div>
+    );
+  };
+
+  if (specialCollections[id] === "_onboarding" && !user.githubUsername) {
+    return (
+      <div className="indicator">
+        <span className="indicator-item badge bg-sky-500 text-black left-0">
+          🛫 start here
+        </span>
+        {/* main card block */}
+        {mainCardBlock()}
+      </div>
+    );
+  } else {
+    return mainCardBlock();
+  }
 };
 
 export default CollectionCard;
