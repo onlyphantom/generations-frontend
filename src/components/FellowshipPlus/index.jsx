58ed7e9fcfbce6fdfaaf2786e2dde0399241c27f
onlyphantom <samuel@outlook.sg>
2023-02-16 19:39:09 +0700

    add FellowshipPlus section

--- src/components/FellowshipPlus/index.jsx
+++ src/components/FellowshipPlus/index.jsx
@@ -1,3 +1,5 @@
+import Checkmark from "../../icons/Checkmark";
+
 const index = () => {
   return (
     <section
@@ -9,6 +11,119 @@ const index = () => {
           Fellowship Plus
         </span>
       </h2>
+
+      <section className="relative overflow-hidden w-full">
+        <div className="relative z-10 container mx-auto">
+          <div className="flex flex-wrap lg:items-center -m-8">
+            <div className="w-full md:w-1/2 p-4">
+              <div>
+                <h2 className="my-4 text-4xl font-bold font-heading tracking-px-n leading-none">
+                  Fellowship+
+                </h2>
+                <h3 className="mb-4 text-xl font-bold font-heading tracking-px-n leading-none">
+                  Accelerate your journey into tech &#38; data.
+                </h3>
+                <p className="prose text-justify">
+                  Taking your first step into the data science and software
+                  engineering industry can be daunting and feel intimidating at
+                  times.
+                  <br />
+                  <h4>Additional Support</h4>
+                  We offer a Fellowship+ membership to pair you with a team of
+                  mentor and a community of peers to help you navigate this
+                  journey. This membership fee is collected to compensate for
+                  the time and effort of all the mentors who are contributing to
+                  your development goals.
+                  <br />
+                  We also use the funds to cover the costs of running the
+                  platform and to provide you with the best experience possible.
+                  <h4>Always Free</h4>
+                  Fellowship+ is an add-on to the core Fellowship program. The
+                  main features of the Fellowship program will always be free to
+                  the greatest extent possible.
+                </p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 py-8">
+              <div className="border md:max-w-md mx-auto overflow-hidden rounded-3xl shadow-8xl">
+                <div className="p-9">
+                  <span className="mb-7 inline-block text-sm text-gray-500 font-semibold uppercase tracking-px">
+                    Features included:
+                  </span>
+                  <ul>
+                    <li className="mb-4 flex items-center">
+                      <Checkmark />
+                      <p className="font-semibold leading-normal">
+                        Peer-to-Peer Learning Forum
+                      </p>
+                    </li>
+                    <li className="mb-4 flex items-center">
+                      <Checkmark />
+                      <p className="font-semibold leading-normal">
+                        Mentorship Access
+                      </p>
+                    </li>
+                    <li className="mb-4 flex items-center">
+                      <Checkmark />
+                      <p className="font-semibold leading-normal">
+                        Learn to Earn
+                      </p>
+                    </li>
+                    <li className="mb-4 flex items-center">
+                      <Checkmark />
+                      <p className="font-semibold leading-normal">
+                        1x Round trip to Malang
+                      </p>
+                    </li>
+                    <li className="flex items-center">
+                      <Checkmark />
+                      <p className="font-semibold leading-normal">
+                        Expedited Email Support
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+                <div className="p-9 bg-white">
+                  <div className="flex flex-wrap -m-8 items-center">
+                    <div className="sm:w-2/3 p-4">
+                      <span className="mb-2 inline-block text-sm text-gray-500 font-semibold uppercase tracking-px">
+                        Fellowship Pro
+                      </span>
+                      <p className="text-gray-900 font-semibold leading-normal">
+                        Unlock all features
+                      </p>
+                    </div>
+                    {/* <div className="w-full sm:w-1/2 p-8"> */}
+                    <div className="sm:w-1/3 p-4">
+                      <div className="sm:max-w-max ml-auto">
+                        <p className="font-bold">
+                          <span className="text-5xl leading-tight tracking-px-n text-secondary">
+                            $12
+                          </span>
+                          <span className="text-lg text-gray-500 leading-snug tracking-px-n">
+                            /mo
+                          </span>
+                        </p>
+                        <p className="font-medium text-gray-500 leading-relaxed text-sm">
+                          Billed anually
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-9">
+                    <button
+                      className="py-4 px-5 w-full text-white font-semibold rounded-xl focus:ring btn-secondary hover:bg-indigo-700 transition ease-in-out duration-200"
+                      type="button"
+                    >
+                      Upgrade to Fellowship+
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </section>
   );
 };
---
+++ src/icons/Checkmark.jsx
@@ -0,0 +1,22 @@
+const Checkmark = () => {
+  return (
+    <svg
+      class="mr-2"
+      width="20"
+      height="20"
+      viewbox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.5 10L9.16667 11.6667L12.5 8.33333M17.5 10C17.5 14.1421 14.1421 17.5 10 17.5C5.85786 17.5 2.5 14.1421 2.5 10C2.5 5.85786 5.85786 2.5 10 2.5C14.1421 2.5 17.5 5.85786 17.5 10Z"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+    </svg>
+  );
+};
+
+export default Checkmark;
