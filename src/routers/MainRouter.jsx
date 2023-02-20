d987daab27b6dc977be5c07e266a12dbd6989c3a
AurelliaChristie <aurelliachristie77@gmail.com>
2023-02-20 14:06:21 +0700

    add payments success page

--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -2,6 +2,7 @@ import Home from '../pages/Home';
 import AuthRedirect from '../pages/AuthRedirect';
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PaymentsSuccess from '../pages/PaymentsSuccess';
 
 function MainRouter() {
   return (
@@ -9,6 +10,7 @@ function MainRouter() {
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="auth/:providerName/redirect" element={<AuthRedirect />} />
+        <Route path="payments/success" element={<PaymentsSuccess />} />
     </Routes>
     </Router>
   );
