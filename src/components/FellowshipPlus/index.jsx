c4b12e6c529697ee5b80ad53751add8d09378eca
AurelliaChristie <aurelliachristie77@gmail.com>
2023-02-24 00:17:12 +0700

    remove extend fellowship plus option, move stripe handler to webhook

--- src/components/FellowshipPlus/index.jsx
+++ src/components/FellowshipPlus/index.jsx
@@ -1,57 +1,66 @@
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
 import PaymentBox from "./PaymentBox";
 
-const index = () => {
-  return (
-    <section
-      id="fellowship-plus"
-      className="mb-32 mx-4 text-center lg:text-left justify-center items-center flex flex-col"
-    >
-      <h2 className="text-4xl font-bold mb-12 text-center">
-        <span className="font-extrabold tracking-tight mb-12 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-          Fellowship Plus
-        </span>
-      </h2>
+const FellowshipPlus = () => {
+  const { u } = useContext(UserContext);
+  const [user] = u;
 
-      <section className="relative overflow-hidden w-full">
-        <div className="relative z-10 container mx-auto">
-          <div className="flex flex-wrap lg:items-center -m-8">
-            <div className="w-full md:w-1/2 p-8">
-              <div>
-                <h2 className="my-4 text-4xl font-bold font-heading tracking-px-n leading-none">
-                  Fellowship+
-                </h2>
-                <h3 className="mb-4 text-xl font-bold font-heading tracking-px-n leading-none">
-                  Accelerate your journey into tech &#38; data.
-                </h3>
-                <p className="prose text-justify">
-                  Taking your first step into the data science and software
-                  engineering industry can be daunting and feel intimidating at
-                  times.
-                  <br />
-                  <h4>Additional Support</h4>
-                  We offer a Fellowship+ membership to pair you with a team of
-                  mentors and a community of peers to help you navigate this
-                  journey. The subscription fee compensates for the time and
-                  effort of all the mentors who are contributing to your
-                  development goals.
-                  <br />
-                  We also use the funds to cover the costs of running the
-                  platform and to provide you with the best experience possible.
-                  <h4>Always Free</h4>
-                  Fellowship+ is an add-on to the core Fellowship program. The
-                  main features of the Fellowship program will always be free to
-                  the greatest extent possible.
-                </p>
+  if(user?.proUser){
+    return <></>;
+  } else {
+    return (
+      <section
+        id="fellowship-plus"
+        className="mb-32 mx-4 text-center lg:text-left justify-center items-center flex flex-col"
+      >
+        <h2 className="text-4xl font-bold mb-12 text-center">
+          <span className="font-extrabold tracking-tight mb-12 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+            Fellowship Plus
+          </span>
+        </h2>
+  
+        <section className="relative overflow-hidden w-full">
+          <div className="relative z-10 container mx-auto">
+            <div className="flex flex-wrap lg:items-center -m-8">
+              <div className="w-full md:w-1/2 p-8">
+                <div>
+                  <h2 className="my-4 text-4xl font-bold font-heading tracking-px-n leading-none">
+                    Fellowship+
+                  </h2>
+                  <h3 className="mb-4 text-xl font-bold font-heading tracking-px-n leading-none">
+                    Accelerate your journey into tech &#38; data.
+                  </h3>
+                  <p className="prose text-justify">
+                    Taking your first step into the data science and software
+                    engineering industry can be daunting and feel intimidating at
+                    times.
+                    <br />
+                    <h4>Additional Support</h4>
+                    We offer a Fellowship+ membership to pair you with a team of
+                    mentors and a community of peers to help you navigate this
+                    journey. The subscription fee compensates for the time and
+                    effort of all the mentors who are contributing to your
+                    development goals.
+                    <br />
+                    We also use the funds to cover the costs of running the
+                    platform and to provide you with the best experience possible.
+                    <h4>Always Free</h4>
+                    Fellowship+ is an add-on to the core Fellowship program. The
+                    main features of the Fellowship program will always be free to
+                    the greatest extent possible.
+                  </p>
+                </div>
+              </div>
+              <div className="w-full md:w-1/2 py-8">
+                <PaymentBox user={user} />
               </div>
             </div>
-            <div className="w-full md:w-1/2 py-8">
-              <PaymentBox />
-            </div>
           </div>
-        </div>
+        </section>
       </section>
-    </section>
-  );
+    );
+  }
 };
 
-export default index;
+export default FellowshipPlus;
--- src/pages/PaymentsSuccess.jsx
+++ src/pages/PaymentsSuccess.jsx
@@ -1,87 +1,87 @@
-import React, { useState, useContext, useEffect } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { UserContext } from "../contexts/UserContext";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const backendURL = "https://generationsapi.herokuapp.com/api";
+// const backendURL = "https://generationsapi.herokuapp.com/api";
 
 function PaymentsSuccess() {
 
     const navigate = useNavigate();
-    const [params] = useSearchParams();
-    const { u } = useContext(UserContext);
-    const [,setUser] = u;
-    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const token = sessionStorage.getItem("userSession");
-        const today = new Date();
-        const sessionId = params.get('sessionId');
-
-        fetch(`${backendURL}/users/add-subscription`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify({
-                sessionId: sessionId,
-                paymentDate: today.toISOString().slice(0,10)
-            }),
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                setUser((prev) => {
-                    return {
-                        ...prev,
-                        proUser: data.proUser,
-                        proExpiry: data.proExpiry
-                    };
-                });
-
-                setLoading(false);
-            });
-    }, [params, setUser])
-        
-    if(loading){
-        return (
-            <div className="w-1/12 mx-auto">
-                <img src="/192.png" alt="logo" />
-                <progress className="progress mt-4"></progress>
-            </div>
-        );
-    } else {  
-        return (
-            <div className="w-3/12 mx-auto">
-                <div className="block rounded-lg shadow-lg bg-glass px-6 py-12 md:px-8 border-solid border-2 border-sky-500">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-8 h-8 mx-auto"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="green"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
+    return (
+        <div className="w-3/12 mx-auto">
+            <div className="block rounded-lg shadow-lg bg-glass px-6 py-12 md:px-8 border-solid border-2 border-sky-500">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8 h-8 mx-auto"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="green"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+                <div className="text-center mt-3">
+                    Payment Successful! 
+                    <button
+                        type="button"
+                        // data-mdb-ripple="true"
+                        // data-mdb-ripple-color="light"
+                        className="mt-3 w-1/2 btn"
+                        onClick={() => navigate("/")}
                     >
-                        <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-                        <polyline points="22 4 12 14.01 9 11.01"></polyline>
-                    </svg>
-                    <div className="text-center mt-3">
-                        Payment Successful! 
-                        <button
-                            type="button"
-                            // data-mdb-ripple="true"
-                            // data-mdb-ripple-color="light"
-                            className="mt-3 w-1/2 btn"
-                            onClick={() => navigate("/")}
-                        >
-                            Back to Home
-                        </button>
-                    </div>
+                        Back to Home
+                    </button>
                 </div>
             </div>
-        )  ;
-    }
+        </div>
+    );
+    // const [params] = useSearchParams();
+    // const { u } = useContext(UserContext);
+    // const [,setUser] = u;
+    // const [loading, setLoading] = useState(true);
+
+    // useEffect(() => {
+    //     const token = sessionStorage.getItem("userSession");
+    //     const today = new Date();
+    //     const sessionId = params.get('sessionId');
+
+    //     fetch(`${backendURL}/users/add-subscription`, {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Authorization: `Bearer ${token}`,
+    //         },
+    //         body: JSON.stringify({
+    //             sessionId: sessionId,
+    //             paymentDate: today.toISOString().slice(0,10)
+    //         }),
+    //     })
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             setUser((prev) => {
+    //                 return {
+    //                     ...prev,
+    //                     proUser: data.proUser,
+    //                     proExpiry: data.proExpiry
+    //                 };
+    //             });
+
+    //             setLoading(false);
+    //         });
+    // }, [params, setUser])
+        
+    // if(loading){
+    //     return (
+    //         <div className="w-1/12 mx-auto">
+    //             <img src="/192.png" alt="logo" />
+    //             <progress className="progress mt-4"></progress>
+    //         </div>
+    //     );
+    // } else {  
+    // }
 }
 
 export default PaymentsSuccess;
