4e87a687b661e0e1dfe06649d65ed9e558325467
onlyphantom <samuel@outlook.sg>
2023-04-21 10:37:06 +0700

    update testimonial

--- src/components/Testimonial/index.js
+++ src/components/Testimonial/index.js
@@ -83,6 +83,27 @@ const Testimonial = () => {
                         <p className="text-sm uppercase">Alumnus, Software Engineer @Boomi</p>
                     </div>
                 </div>
+                <div className="flex flex-col max-w-lg mx-4 my-4 shadow-lg">
+                    <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
+                        <p className="relative px-6 py-1 text-lg italic text-center dark:text-gray-100">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 dark:text-violet-400">
+                                <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                                <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                            </svg>Samuel is by far the most motivational and cross-functional educator I have had. He cares for each
+                            of his students and tailor each learning experience to their needs. The mentors offer different perspectives
+                            diligently and show their range and compassion in the field. Thanks Sam.
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 dark:text-violet-400">
+                                <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                                <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                            </svg>
+                        </p>
+                    </div>
+                    <div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-secondary text-gray-300">
+                        <img src="https://media.licdn.com/dms/image/C5603AQE7vjwuWOl6cA/profile-displayphoto-shrink_100_100/0/1647219687485?e=1687392000&v=beta&t=YZc2SGGuYLnp2ltqll4gIGWo3BX6SY5zRvwFq4Fb8Uw" alt="Vincent supertype fellowship endorsement" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700" />
+                        <p className="text-xl font-semibold leading-tight">Vincent Rueb</p>
+                        <p className="text-sm uppercase">Alumnus, Web 3 Artist &amp; Entrepreneur</p>
+                    </div>
+                </div>
             </div>
         </section>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
     "./public/index.html",
   ],
   purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
     content: [
       './src/components/**/*.{js,jsx,ts,tsx}',
