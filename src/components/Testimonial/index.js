ab42b8e53215d1df769727f40b08d7bea2fb6891
Samuel Chan <samuel@outlook.sg>
2024-02-26 22:07:54 +0700

    add testimonial section

--- src/components/Testimonial/index.js
+++ src/components/Testimonial/index.js
@@ -71,7 +71,7 @@ const Testimonial = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 dark:text-violet-400">
                                 <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
                                 <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-                            </svg>I want to see this program grow and really it&apos;s been a privilege to be a part of a team that&apos;s so communicative and being under you as a mentee has been a great experience
+                            </svg>I had the pleasure of coming across Samuel&apos;s insightful YouTube tutorials on developing Large Language Models (LLMs) projects. His lucid teaching approach and in-depth knowledge resonated with me, and I reached out to him for mentorship. I&apos;m grateful that he agreed and has been consistently providing invaluable insights since day one. Samuel is a dynamo, always buzzing with energy and brimming with ideas, and he possesses an innate ability to inspire others. He brings a full-stack perspective to his work, and his comprehensive understanding of each layer has been a great learning resource for me. What&apos;s truly impressive about Samuel is his knack for aligning my personal career goals with practical projects. These projects, while serving as effective learning platforms, have also helped me get hands-on experience and become more proficient in my field. Samuel goes above and beyond by providing not only strategic guidance but also assisting in the details. This balance between the big picture and the nitty-gritty has greatly helped me to holistically grow my skills. I unequivocally recommend Samuel. His depth of knowledge, ability to motivate others, and commitment to going the extra mile will undoubtedly make him a tremendous asset wherever he lands.
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 dark:text-violet-400">
                                 <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
                                 <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
@@ -79,9 +79,10 @@ const Testimonial = () => {
                         </p>
                     </div>
                     <div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-secondary text-gray-300">
-                        <img src="/testimonial_img/kshitij.jpeg" alt="Kshitij supertype fellowship endorsement" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700" />
-                        <p className="text-xl font-semibold leading-tight">Kshitij Bir Arora</p>
-                        <p className="text-sm uppercase">Alumnus, Software Engineer @Boomi</p>
+                        <img src="/testimonial_img/jiayang.jpeg"
+                            alt="jiayang supertype fellowship endorsement" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700" />
+                        <p className="text-xl font-semibold leading-tight">Jiayang Cheng</p>
+                        <p className="text-sm uppercase">Engineering Analyst, Google / YouTube</p>
                     </div>
                 </div>
                 <div className="flex flex-col max-w-lg mx-4 my-4 shadow-lg">
@@ -124,6 +125,46 @@ const Testimonial = () => {
                             alt="Vincent supertype fellowship endorsement" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700" />
                         <p className="text-xl font-semibold leading-tight">Vincent Rueb</p>
                         <p className="text-sm uppercase">Alumnus, Web 3 Artist &amp; Entrepreneur</p>
+                    </div>
+                </div>
+                <div className="flex flex-col max-w-lg mx-4 my-4 shadow-lg">
+                    <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
+                        <p className="relative px-6 py-1 text-lg italic text-center dark:text-gray-100">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 dark:text-violet-400">
+                                <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                                <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                            </svg>
+                            With Samuel I started my journey in programming, and despite his mastery in this domain, he had the courage and capabilities to teach me through all of the beginning, to the point where I can start working on my own.
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 dark:text-violet-400">
+                                <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                                <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                            </svg>
+                        </p>
+                    </div>
+                    <div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-primary">
+                        <img src="/testimonial_img/nurit.jpeg" alt="nurit supertype fellowship review" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700" />
+                        <p className="text-xl font-semibold leading-tight">Nurit Ehrismann</p>
+                        <p className="text-sm uppercase">Analyst, Credit Suisse</p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col max-w-lg mx-4 my-4 shadow-lg">
+                    <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
+                        <p className="relative px-6 py-1 text-lg italic text-center dark:text-gray-100">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 dark:text-violet-400">
+                                <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                                <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                            </svg>I want to see this program grow and really it&apos;s been a privilege to be a part of a team that&apos;s so communicative and being under you as a mentee has been a great experience
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 dark:text-violet-400">
+                                <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                                <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                            </svg>
+                        </p>
+                    </div>
+                    <div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-secondary text-gray-300">
+                        <img src="/testimonial_img/kshitij.jpeg" alt="Kshitij supertype fellowship endorsement" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700" />
+                        <p className="text-xl font-semibold leading-tight">Kshitij Bir Arora</p>
+                        <p className="text-sm uppercase">Alumnus, Software Engineer @Boomi</p>
                     </div>
                 </div>
 
