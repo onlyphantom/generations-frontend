a0ed57288324a735d7c6b16263ebd26b8ddc5efb
AurelliaChristie <aurelliachristie77@gmail.com>
2024-03-15 17:29:36 +0700

    fix payment bug

--- src/components/FellowshipPlus/PaymentBox.jsx
+++ src/components/FellowshipPlus/PaymentBox.jsx
@@ -102,7 +102,7 @@ const PaymentBox = ({ user }) => {
               user?.token && !user?.proUser ? "" : "hidden "
             }SS_ProductCheckout py-4 px-5 lg:text-sm w-full text-white font-semibold rounded-xl focus:ring btn-secondary transition ease-in-out duration-200`}
             type="button"
-            data-id="4"
+            data-id="7"
             data-email={user?.email}
             data-url="https://generationsapi.herokuapp.com"
           >
